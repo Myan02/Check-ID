059c7be3404431ee6ac72e275c08babf25c38e6d
Michael Baburyan <baburyanmichael@gmail.com>
2023-09-27 01:30:26 -0400

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module com.among_us {
-    requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-    opens com.among_us to javafx.fxml;
-    exports com.among_us;
-}
